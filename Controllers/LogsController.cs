30a1cce2ddace15aea635df436e283057e24bac7
alxac <alxac@msn.com>
2024-12-03 17:14:22 -0400

    Adicionar arquivos de projeto.

---
+++ Controllers/LogsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using TechNation.Services;
+
+namespace TechNation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LogsController : ControllerBase
+    {
+        private readonly LogConverterService _logConverterService = new LogConverterService();
+
+        [HttpPost("convert")]
+        public async Task<IActionResult> ConvertLog([FromBody] string logContent)
+        {
+            var convertedLog = _logConverterService.ConvertLog(logContent);
+            return Ok(convertedLog);
+        }
+
+        [HttpPost("convertFromFile")]
+        public async Task<IActionResult> ConvertLogFromFile([FromForm] IFormFile file)
+        {
+            if (file == null)
+                return Ok("Erro");
+            var reader = new StreamReader(file.OpenReadStream());
+            var logContent = await reader.ReadToEndAsync();
+            var convertedLog = _logConverterService.ConvertLog(logContent);
+            return Ok(convertedLog);
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Logs", "Logs" };
+        }
+    }
+}
---
+++ Services/LogConverterService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TechNation.Services
+{
+    public class LogConverterService
+    {
+        public string ConvertLog(string inputLog)
+        {
+            var lines = inputLog.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            var outputLog = new List<string>
+            {
+                "#Version: 1.0",
+                $"#Date: {DateTime.UtcNow:dd/MM/yyyy HH:mm:ss}",
+                "#Fields: provider http-method status-code uri-path time-taken response-size cache-status"
+            };
+
+            foreach (var line in lines)
+            {
+                var parts = line.Split('|');
+                if (parts.Length == 5)
+                {
+                    string cacheStatus;
+                    switch (parts[2])
+                    {
+                        case "HIT":
+                            cacheStatus = "HIT";
+                            break;
+                        case "MISS":
+                            cacheStatus = "MISS";
+                            break;
+                        case "INVALIDATE":
+                            cacheStatus = "REFRESH_HIT";
+                            break;
+                        default:
+                            cacheStatus = parts[2];
+                            break;
+                    }
+
+                    var methodUriParts = parts[3].Split(' ');
+                    outputLog.Add($"\"MINHA CDN\" {methodUriParts[0]} {parts[1]} {methodUriParts[1]} {Math.Round(Convert.ToDouble(parts[4]), 0)} {parts[0]} {cacheStatus}");
+                }
+            }
+
+            return string.Join("\n", outputLog);
+        }
+    }
+}
